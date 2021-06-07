7c1fd71ddaa61cbe8cf56af529c0ee9a31c8f19e
Quazi Adibur Rahman Adib <78149599+quaziadib@users.noreply.github.com>
2021-06-07 22:19:40 +0600

    3.0 changes

--- Detector.py
+++ Detector.py
@@ -8,14 +8,14 @@ def rescaleFrame(frame, scale=0.75):
     dimensions = (width, height)
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
-red_lo_1 = (0, 100, 100)
-red_hi_1 = (10, 255, 255)
+# red_lo_1 = (0, 100, 100)
+# red_hi_1 = (10, 255, 255)
 
-red_lo_2 = (160, 100, 100)
-red_hi_2 = (179, 255, 255)
+red_lo_2 = (166, 159, 90)
+red_hi_2 = (180, 255, 255)
 
 
-capture = cv.VideoCapture(0)
+capture = cv.VideoCapture(0) # used 'picture/Video.mp4' for testing
 
 while True:
     isTrue, frame = capture.read()
@@ -24,26 +24,28 @@ while True:
         break
 
     frame_resz = rescaleFrame(frame)
-    blurred = cv.GaussianBlur(frame_resz, (7, 7), cv.BORDER_DEFAULT)
+    blurred = cv.GaussianBlur(frame_resz, (3, 3), cv.BORDER_CONSTANT)
     col_hsv = cv.cvtColor(blurred, cv.COLOR_BGR2HSV)
-    mask_1 = cv.inRange(col_hsv, red_lo_1, red_hi_1)
+    #mask_1 = cv.inRange(col_hsv, red_lo_1, red_hi_1)
     mask_2 = cv.inRange(col_hsv, red_lo_2, red_hi_2)
-    mask = cv.addWeighted(mask_2, 1.0, mask_1, 1.0, 0.0)
-    mask = cv.erode(mask, (7, 7), iterations=2)
-    mask = cv.dilate(mask, (7, 7), iterations=2)
-    contors = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    #mask = cv.addWeighted(mask_2, 1.0, mask_1, 1.0, 0.0)
+    mask = cv.erode(mask_2,None, iterations=6)
+    mask = cv.dilate(mask_2, None, iterations=6)
+
+    contors = cv.findContours(mask_2, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     contors = imutils.grab_contours(contors)
 
     if len(contors) > 0:
         large = max(contors, key = cv.contourArea)
         ((x,y), radius) = cv.minEnclosingCircle(large)
         moms = cv.moments(large)
+        flag = 1
         try:
             center = (int(moms['m10']/moms['m00']), int(moms['m01']/moms['m00']))
         except ZeroDivisionError:
-            pass
+            flag = 0
 
-        if radius > 10:
+        if radius > 10 and flag:
             cv.circle(frame_resz, (int(x), int(y)), int(radius),(0, 255, 0), 2)
 
 
